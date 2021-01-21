5e63ed23ca53d0d8c77194198933e73aa5d1d5cc
thb007 <18780475970@163.com>
2021-01-21 18:00:24 +0800

    feat: nothing

--- app/src/main/cpp/test/MyGLRenderContext.cpp
+++ app/src/main/cpp/test/MyGLRenderContext.cpp
@@ -18,7 +18,7 @@
 MyGLRenderContext *MyGLRenderContext::m_pContext = nullptr;
 
 MyGLRenderContext::MyGLRenderContext() {
-    m_Sample = generateSample(2);
+    m_Sample = generateSample(1);
 }
 
 GLBaseSample *MyGLRenderContext::generateSample(int type) {
