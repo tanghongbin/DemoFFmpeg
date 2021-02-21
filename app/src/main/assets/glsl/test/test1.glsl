9d97a99c67f3e67cf99c2698ce6100ea594fbe11
thb <18780475970@163.com>
2021-02-21 22:10:13 +0800

    feat: 顶点属性设置

---
+++ app/src/main/assets/glsl/test/test1.glsl
@@ -0,0 +1,15 @@
+#version 300 es
+
+struct test{
+    vec3 abi;
+    float abi2;
+};
+const float pi = 1.0f;
+vec4 matt;
+uniform mat3 test2;
+in int test3;
+
+void main() {
+    int result = test3 * 1;
+    test a = test(vec3(1.0,2.0,3.0),1.0);
+}
--- app/src/main/cpp/sample/TriangleSample.cpp
+++ app/src/main/cpp/sample/TriangleSample.cpp
@@ -18,20 +18,24 @@ TriangleSample::~TriangleSample() {
 
 void TriangleSample::init() {
     char vShaderStr[] =
-            "#version 300 es                          \n"
+            "#version 300 es                   \n"
+            "layout(location = 1) in vec4 a_color;       \n"
             "layout(location = 0) in vec4 vPosition;  \n"
+            "out vec4 v_color; \n"
             "void main()                              \n"
-            "{                                        \n"
+            "{                         \n"
+            "   v_color = a_color;            \n"
             "   gl_Position = vPosition;              \n"
             "}                                        \n";
 
     char fShaderStr[] =
             "#version 300 es                              \n"
-            "precision mediump float;                     \n"
+            "precision mediump float;   \n"
+            "in vec4 v_color;                     \n"
             "out vec4 fragColor;                          \n"
             "void main()                                  \n"
             "{                                            \n"
-            "   fragColor = vec4 ( 1.0, 0.0, 0.0, 1.0 );  \n"
+            "   fragColor = v_color;  \n"
             "}                                            \n";
 
     GLuint m_VertexShader;
@@ -48,6 +52,12 @@ void TriangleSample::draw() {
             0.5f, -0.5f, 0.0f,
     };
 
+//    GLfloat vVertices2[] = {
+//            1.0f, 0.5f, 0.0f,
+//            -0.5f, -0.5f, 0.0f,
+//            0.5f, -0.5f, 0.0f,
+//    };
+
     if (m_ProgramObj == 0)
         return;
 
@@ -55,10 +65,19 @@ void TriangleSample::draw() {
     glUseProgram(m_ProgramObj);
 
     // Load the vertex data
+    GLfloat colors1[12] = {1.0f,0.0f,0.0f,1.0f,
+                          0.0f,1.0f,0.0f,1.0f,
+                          0.0f,0.0f,1.0f,1.0f};
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, vVertices);
     glEnableVertexAttribArray(0);
 
+
+    glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 0, colors1);
+    glEnableVertexAttribArray(1);
+
+
     glDrawArrays(GL_TRIANGLES, 0, 3);
+    glDisableVertexAttribArray(0);
 
 }
 
