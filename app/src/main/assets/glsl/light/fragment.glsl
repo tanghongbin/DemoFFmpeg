7442004adc61e6713adb27d4a887f04ef7299c3b
thb007 <18780475970@163.com>
2021-04-01 15:16:55 +0800

    feat: 增加材质和摄像机视角

--- app/src/main/assets/glsl/light/fragment.glsl
+++ app/src/main/assets/glsl/light/fragment.glsl
@@ -7,23 +7,28 @@ uniform vec3 objectColor;
 uniform vec3 lightColor;
 uniform vec3 lightPos;
 uniform vec3 viewPos;
+struct Material{
+  vec3 ambient;
+  vec3 diffuse;
+  vec3 specular;
+  float shininess;
+};
+uniform Material material;
 void main()
 {
-  float ambientStrength = 0.1f;
-  vec3 ambient = lightColor * ambientStrength;
+  vec3 ambient = lightColor * material.ambient;
   vec3 norm = normalize(Normal);
   vec3 lightDir = normalize(lightPos - FragPos);
 //    vec3 result = (ambient) * objectColor;
   // 计算漫反射角度
   float diff= max(dot(norm,lightDir),0.0);
-  vec3 diffuse = diff * lightColor;
+  vec3 diffuse = material.diffuse * diff * lightColor;
 //  vec3 result = (ambient + diffuse) * objectColor;
   // 计算镜面强度
-  float specularStrength = 0.5;
   vec3 viewDir = normalize(viewPos - FragPos);
   vec3 reflectDir = reflect(-lightDir,norm);
-  float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32.0);
-  vec3 specular = specularStrength * spec * lightColor;
+  float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+  vec3 specular = material.specular * spec * lightColor;
   vec3 result = (ambient + diffuse + specular) * objectColor;
   outColor = vec4(result,1.0);
 }
--- app/src/main/cpp/sample/LightSample.cpp
+++ app/src/main/cpp/sample/LightSample.cpp
@@ -5,6 +5,7 @@
 
 #include "LightSample.h"
 #include <GLES3/gl3.h>
+#include <camera.h>
 #include "stb_image.h"
 #include "CustomGLUtils.h"
 #include "ImageDef.h"
@@ -103,29 +104,43 @@ void LightSample::draw() {
     if (second == 0L){
         second = GetSysCurrentTime();
     }
-    second = (GetSysCurrentTime() - second) / 1000;
-    glm::vec3 lightPos = glm::vec3( 1.2f,  1.0f,  1.0f);
-    glm::vec3 cameraPos = glm::vec3(0.0f,0.0f,-5.0f);
+    float diffValue = (float)(GetSysCurrentTime() - second) / (float)1000;
+    LOGCATE("log second:%f",diffValue);
+    glm::vec3 lightPos = glm::vec3( 1.2f,  1.3f,  3.0f);
+    float radius = 8.0;
+    float camX = sin(diffValue) * radius;
+//    float camZ = sin(diffValue) * radius;
+//    if (camZ <= 3.0){
+//        camZ = 3.0;
+//    }
+    glm::vec3 cameraPos = glm::vec3(2.0,1.3f,6.0);
+//    glm::vec3 cameraPos = glm::vec3(0.0,0.0f,3.0);
+    glm::vec3 camreaFront = glm::vec3 (0.0,0.0,-1.0);
+    glm::vec3 cameraUp    = glm::vec3(0.0f, 1.0f,  0.0f);
 //    LOGCATE("imagewidth:%i   ---imageHeight:%i",m_RenderImage.width,m_RenderImage.height);
     // Use the program object
+    Camera camera(cameraPos);
 
     glUseProgram (m_ProgramObj);
     glEnable(GL_DEPTH_TEST);
     glBindVertexArray(lightVao);
     setVec3(m_ProgramObj,"lightPos",lightPos);
-    setVec3(m_ProgramObj,"viewPos",cameraPos);
+    setVec3(m_ProgramObj,"viewPos",camera.Position);
     model = glm::mat4(1.0f);
-    view = glm::mat4(1.0f);
     projection = glm::mat4(1.0f);
     model = glm::translate(model, glm::vec3( 0.0f,  0.0f,  0.0f));
-    model = glm::rotate(model, 5.0f, glm::vec3(1.0f, 0.3f, 0.0f));
-    view = glm::translate(view,cameraPos);
+//    model = glm::rotate(model, 5.0f, glm::vec3(1.0f, 0.3f, 0.0f));
+    view = camera.GetViewMatrix();
     projection = glm::perspective(glm::radians(45.0f),1.33f,0.1f,100.0f);
     setMat4(m_ProgramObj, "model", model);
     setMat4(m_ProgramObj, "view", view);
     setMat4(m_ProgramObj, "projection", projection);
     setVec3(m_ProgramObj,"objectColor",glm::vec3(1.0f,0.5f,0.31f));
     setVec3(m_ProgramObj,"lightColor",glm::vec3(1.0f,1.0f,1.0f));
+    setVec3(m_ProgramObj,"material.ambient",1.0f,0.5f,0.31f);
+    setVec3(m_ProgramObj,"material.diffuse",1.0f,0.5f,0.31f);
+    setVec3(m_ProgramObj,"material.specular",0.5,0.5,0.5);
+    setFloat(m_ProgramObj,"material.shininess",32.0);
     glDrawArrays(GL_TRIANGLES, 0, 36);
 
 
@@ -133,12 +148,9 @@ void LightSample::draw() {
     glEnable(GL_DEPTH_TEST);
     glBindVertexArray(lightVao);
     model = glm::mat4();
-    view = glm::mat4(1.0f);
     projection = glm::mat4(1.0f);
-
     model = glm::translate(model, lightPos);
     model = glm::scale(model,  glm::vec3(0.2f));
-    view = glm::translate(view,cameraPos);
     projection = glm::perspective(glm::radians(45.0f),1.33f,0.1f,100.0f);
     setMat4(lightProgram, "model", model);
     setMat4(lightProgram, "view", view);
