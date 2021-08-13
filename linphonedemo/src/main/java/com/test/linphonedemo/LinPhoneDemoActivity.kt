8c605353ade9419b8b0589bdfc2f95c66e3005ee
thb007 <18780475970@163.com>
2021-08-13 10:55:38 +0800

    feat: 放弃linphone的学习，转战webrtc

---
+++ linphonedemo/src/main/java/com/test/linphonedemo/LinPhoneDemoActivity.kt
@@ -0,0 +1,11 @@
+package com.test.linphonedemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LinPhoneDemoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lin_phone_demo)
+    }
+}
